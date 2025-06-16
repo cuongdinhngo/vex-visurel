f12c7d6687561e8a1ae78eeb0fe19e5455f5380d
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-16 23:14:01 +0700

    feat: Breadcrumbs

---
+++ components/items/breadcrumbs/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-breadcrumbs
+    :items="items"
+    style="text-decoration: none;"
+  >
+    <template v-slot:divider>
+      <v-icon icon="mdi-square-small"></v-icon>
+    </template>
+    <template v-slot:title="{ item }">
+      {{ item.title }}
+    </template>
+  </v-breadcrumbs>
+</template>
+<script setup lang="ts">
+const items = [
+  {
+    title: 'Home',
+    disable: false,
+    icon: 'mdi-home',
+    to: { name: 'index'}
+  },
+  {
+    title: 'Dashboards',
+    disable: true,
+    icon: 'mdi-view-dashboard',
+    to: { name: 'index'}
+  },
+  {
+    title: 'Analytics',
+    disable: true,
+    icon: 'mdi-cog',
+    to: { name: 'index'}
+  }
+];
+</script>
