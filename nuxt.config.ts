9ee18a1197b824e5d7531eb4d5ba9e3148005ce5
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-16 09:10:32 +0700

    feat: font & color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   ssr: false,
-  modules: ['vuetify-nuxt-module'],
-    app: {
+  modules: [
+    'vuetify-nuxt-module',
+    '@vueuse/nuxt'
+  ],
+  app: {
     baseURL: '/vex-visurel',
     head: {
       link: [
@@ -12,8 +15,28 @@ export default defineNuxtConfig({
           rel: 'icon',
           type: 'image/svg+xml',
           href: '/logo.svg'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
         }
       ]
     }
   },
+  css: [
+    '@/assets/styles/main.scss',
+  ],
+  vuetify: {
+    moduleOptions: {},
+    vuetifyOptions: './vuetify.config.ts'
+  }
 })
---
+++ vuetify.config.ts
@@ -0,0 +1,44 @@
+import { defineVuetifyConfiguration } from 'vuetify-nuxt-module/custom-configuration'
+
+export default defineVuetifyConfiguration({
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: '#4F46E5',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+          background: '#EEF5F9', // CUSTOM light background
+          surface: '#FFFFFF',
+          'on-background': '#000000',
+          'on-surface': '#3e3d3d',
+        }
+      },
+      dark: {
+        dark: true,
+        colors: {
+          primary: '#4F46E5',
+          secondary: '#03DAC6',
+          accent: '#03DAC5',
+          error: '#CF6679',
+          info: '#6200EE',
+          success: '#03DAC5',
+          warning: '#FF9800',
+          background: '#1F2937', // Dark background
+          surface: '#FFFFFF',
+          'on-background': '#000000',
+          'on-surface': '#3e3d3d',
+        }
+      }
+    }
+  },
+  icons: {
+    defaultSet: 'mdi'
+  }
+});
