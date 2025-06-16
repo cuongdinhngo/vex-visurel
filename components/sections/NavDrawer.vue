255ea529f0fd922de7b0840d9b0d85cf1e11d4c5
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-16 13:30:16 +0700

    feat: Main nav drawer

---
+++ components/sections/NavDrawer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    app
+    density="compact"
+    nav
+    color="#1F2937"
+  >
+    <v-list>
+      <v-list-item to="/">
+        <v-list-item-title class="">Home</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/about">
+        <v-list-item-title>About</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script setup lang="ts">
+const drawer = defineModel('drawer', {
+  default: true,
+  type: Boolean,
+})
+</script>
