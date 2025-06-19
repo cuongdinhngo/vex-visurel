b3bba7b3149904202a2c252422da1683f3771228
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-19 16:29:23 +0700

    feat: Timeline

---
+++ components/social/Timeline.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- Left Content -->
+  <v-col cols="12" md="8">
+    <!-- Photos -->
+    <SocialPhotos />
+  </v-col>
+
+  <!-- Right Suggestions -->
+  <v-col cols="12" md="4">
+    <!-- Personal Info -->
+    <SocialAbout
+      :breakpoints="{
+        sm: 12,
+        md: 12,
+        lg: 12
+      }"
+    />
+
+    <!-- Friend Suggestions -->
+    <SocialFriendSuggestions />
+  </v-col>
+</template>
---
+++ pages/social/timeline.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <!-- Profile Photo -->
+    <v-row>
+      <v-col cols="12">
+        <SocialProfilePhoto />
+      </v-col>
+    </v-row>
+
+    <!-- Main Content -->
+    <v-row>
+       <SocialTimeline />
+    </v-row>
+  </v-container>
+</template>
