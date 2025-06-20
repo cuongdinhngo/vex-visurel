c9d5b3e7aed5d4179708582f769087d7d23c9efe
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-20 14:01:06 +0700

    feat: Contact Table page

--- pages/contacts/table.vue
+++ pages/contacts/table.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid class="pa-0 ma-0 ">
+  <v-container fluid class="pa-0 ma-0 bg-transparent wrapper">
     <!-- Header row -->
     <v-row class="header-container justify-center" width="100%" no-gutters>
       <v-col cols="12" md="10">
@@ -8,20 +8,27 @@
             cols="12" md="4"
             class="d-flex align-center"
           >
-            <v-avatar
-              class="mr-3 pa-6 bg-deep-purple-lighten-5"
-            >
+              <v-avatar
+                v-if="!mobile"
+                class="mr-3 pa-6 bg-deep-purple-lighten-5"
+              >
+                <v-icon
+                  icon="mdi-contacts-outline"
+                  size="30"
+                  color="deep-purple-darken-2"
+                ></v-icon>
+              </v-avatar>
               <v-icon
-                icon="mdi-contacts-outline"
-                size="30"
-                color="deep-purple-darken-2"
-              ></v-icon>
-            </v-avatar>
-            <span class="text-h4 font-weight-bold text-black">Contacts</span>
+                class="mx-4"
+                v-if="mobile"
+                @click="contactDrawer = !contactDrawer"
+              >mdi-menu</v-icon>
+              <span class="text-h4 font-weight-bold text-black">Contacts</span>
           </v-col>
 
           <v-col
             cols="12" md="8"
+            v-if="!mobile"
           >
             <v-text-field
               prepend-inner-icon="mdi-magnify"
@@ -33,8 +40,8 @@
               elevation="0"
               flat
               density="compact"
-              width="60%"
               class="mx-auto"
+              width="60%"
             ></v-text-field>
           </v-col>
         </v-row>
@@ -42,49 +49,138 @@
     </v-row>
 
     <!-- Main content -->
-    <v-row no-gutters class="">
-      <v-layout>
-        <v-navigation-drawer
-          permanent
-          app
-          width="250"
-          class="bg-transparent"
-        >
-          <div class="header-navigation-drawer d-flex justify-center">
-            <v-btn
-              variant="flat"
-              color="primary"
-            >Compose</v-btn>
-          </div>
-          <v-list>
-            <v-list-item
-              v-for="tabItem in tabs"
-              :key="tabItem.value"
-            >
-              <v-list-item-title>{{ tabItem.title }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-        <v-main height="76vh" class="pb-2 ma-0">
-          <v-row class="justify-center">
-            <v-col cols="12" md="10">
+    <v-row no-gutters class="justify-center main-content">
+      <v-col cols="12" md="11">
+        <v-layout>
+          <v-navigation-drawer
+            v-model="contactDrawer"
+            app
+            width="280"
+            :class="[mobile ? 'bg-white' : 'bg-transparent']"
+            floating
+          >
+            <div class="d-flex justify-center my-2">
+              <v-btn
+                variant="flat"
+                color="primary"
+                prepend-icon="mdi-plus"
+                class="text-none rounded-pill"
+              >Add Contact</v-btn>
+            </div>
+            <v-list class="mt-6 ml-4">
+              <v-list-item
+                v-for="(tabItem, index) in tabs"
+                :key="index"
+                :value="tabItem.value"
+                :prepend-icon="tabItem.icon"
+                density="compact"
+                slim
+                color="primary"
+                variant="text"
+                class="text-black rounded-lg my-1"
+                width="90%"
+              >
+                <v-list-item-title>{{ tabItem.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-navigation-drawer>
 
-            </v-col>
-          </v-row>
-        </v-main>
-      </v-layout>
+          <v-main height="80vh" class="ml-4">
+            <v-row class="justify-center">
+              <v-col cols="12" md="12" class="">
+                <v-data-table
+                  :items="contacts"
+                  :headers="headers"
+                  item-value="id"
+                  hover
+                  class="border-thin text-black"
+                  height="650"
+                  fixed-header
+                  width="100%"
+                >
+                  <template v-slot:item.isSelected="{ item }">
+                    <v-checkbox-btn
+                      color="primary"
+                      @click.native="item.isSelected = !item.isSelected"
+                    ></v-checkbox-btn>
+                  </template>
+
+                  <template v-slot:item.avatar="{ item }">
+                    <v-avatar size="40" class="mr-2">
+                      <img :src="item.avatar" alt="Avatar">
+                    </v-avatar>
+                  </template>
+
+                  <template v-slot:item.isStar="{ item }">
+                    <v-rating
+                      :model-value="item.isStar"
+                      :length="1"
+                      active-color="orange-lighten-1"
+                      size="md"
+                      clearable
+                    ></v-rating>
+                  </template>
+
+                  <template v-slot:item.actions="{ item }">
+                    <v-btn
+                      variant="text"
+                      icon
+                      size="small"
+                    >
+                      <v-icon>mdi-dots-vertical</v-icon>
+                      <v-menu activator="parent">
+                        <v-list>
+                          <v-list-item
+                            v-for="(item, index) in actions"
+                            :key="index"
+                            :value="index"
+                            :prepend-icon="item.icon"
+                            class="text-black"
+                            density="compact"
+                            nav
+                            slim
+                          >
+                            <v-list-item-title>{{ item.title }}</v-list-item-title>
+                          </v-list-item>
+                        </v-list>
+                      </v-menu>
+                    </v-btn>
+                  </template>
+                </v-data-table>
+              </v-col>
+            </v-row>
+          </v-main>
+        </v-layout>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 <script setup lang="ts">
 import { faker } from '@faker-js/faker';
-const tab = ref(null);
+
+const { mobile } = useDisplay();
+const contactDrawer = ref(true);
+
+const actions = [
+  { title: 'Edit', icon: 'mdi-pencil' },
+  { title: 'Delete', icon: 'mdi-delete' },
+];
 
 const tabs = [
   { title: 'All Contacts', value: 'all', icon: 'mdi-account-multiple' },
   { title: 'Frequently Contacted', value: 'frequently', icon: 'mdi-account-star' },
   { title: 'Starred', value: 'starred', icon: 'mdi-star' },
 ];
+
+const headers = [
+  { title: '', align: 'center', sortable: false, key: 'isSelected' },
+  { title: '', align: 'center', sortable: false, key: 'avatar' },
+  { title: 'Name', align: 'start', key: 'name' },
+  { title: 'Email', align: 'start', key: 'email' },
+  { title: 'City', align: 'start', key: 'city' },
+  { title: '', align: 'center', key: 'isStar' },
+  { title: '', align: 'center', key: 'actions' },
+]
 
 const contacts = Array.from({ length: 20 }, () => ({
   id: faker.string.uuid(),
@@ -94,6 +190,7 @@ const contacts = Array.from({ length: 20 }, () => ({
   avatar: `https://picsum.photos/seed/${Math.floor(Math.random() * 100)}/200/300`,
   isStar: faker.number.int({ min: 0, max: 1 }),
   counts: faker.number.int({ min: 0, max: 100 }),
+  isSelected: false,
 }));
 
 const starredContacts = computed(() => {
@@ -107,10 +204,21 @@ const frequentlyContacts = computed(() => {
 <style scoped>
 .header-container {
   background-color: #4f46e51a;
-  padding: 37px 0;
+  padding-top: 40px;
+  padding-bottom: 100px;
 }
 
 .header-navigation-drawer {
   background-color: #4f46e51a;
 }
+
+.wrapper {
+  position: relative;
+}
+
+.main-content {
+  position: relative;
+  top: -55px;
+}
+
 </style>
